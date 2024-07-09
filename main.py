478f997befa329263bcb203f6a49e266796550ac
davismiler <0xaliasrowyan@gmail.com>
2024-07-09 17:03:42 +0100

    starting point

---
+++ main.py
@@ -0,0 +1,11 @@
+# region imports
+from AlgorithmImports import *
+# endregion
+
+class SharedProject(QCAlgorithm):
+
+    def initialize(self):
+        pass
+
+    def on_data(self, data: Slice):
+        pass
