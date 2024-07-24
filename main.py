d64a14039e9e703e6934ffc4bdabca17a6b7afb6
davismiler <0xaliasrowyan@gmail.com>
2024-07-24 13:20:44 +0100

    integrated a neural network to predict price movements and scheduled events to prevent errors

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # region imports
 from AlgorithmImports import *
+from tensorflow.keras.models import Sequential
+import json
 # endregion
 
 class SharedProject(QCAlgorithm):
@@ -8,17 +10,39 @@ class SharedProject(QCAlgorithm):
         self.set_start_date(2022, 1, 1)
         self.set_cash(100000)
 
+        model_key = 'forex_price_predictor'
+        if self.ObjectStore.ContainsKey(model_key):
+            model_str = self.ObjectStore.Read(model_key)
+            config = json.loads(model_str)['config']
+            self.model = Sequential.from_config(config)
+
         self.xauusd = self.add_cfd("XAUUSD", Resolution.MINUTE, Market.OANDA).symbol
         self.bb = self.BB(self.xauusd, 20, 2)
         self.rsi = self.RSI(self.xauusd, 20)
         self.adx = self.ADX(self.xauusd, 20)
 
         # operates on a 30 min time frame
-        self.consolidate("XAUUSD", timedelta(minutes=15), self.on_30_data)
+        self.consolidate("XAUUSD", timedelta(minutes=30), self.on_30_data)
 
         # creates a stop loss
         self.exit_price = None
         self.percentage_risk = 0.02
+
+        # enable trading 40 mins after market open
+        self.Schedule.On(
+            self.DateRules.EveryDay(self.xauusd),
+            self.TimeRules.after_market_open(self.xauusd, 40),
+            self.enable_trading
+        )
+
+        # disable trading 5 mins before market closes
+        self.Schedule.On(
+            self.DateRules.EveryDay(self.xauusd),
+            self.TimeRules.before_market_close(self.xauusd, 5),
+            self.disable_trading
+        )
+
+        self.is_trading_enabled = False
 
         # creates plot of portfolio activity 
         stock_plot = Chart("Trade Plot")
@@ -34,7 +58,7 @@ class SharedProject(QCAlgorithm):
         self.add_chart(stock_plot)
 
     def on_30_data(self, bar):
-        if not self.bb.is_ready or not self.rsi.is_ready or not self.adx.is_ready:
+        if not self.bb.is_ready or not self.rsi.is_ready or not self.adx.is_ready or not self.is_trading_enabled:
             return
 
         price = bar.close
@@ -45,11 +69,11 @@ class SharedProject(QCAlgorithm):
         self.plot("Trade Plot", "LowerBand", self.bb.lower_band.current.value)
 
         if not self.portfolio.invested:
-            if self.bb.lower_band.current.value > price and self.rsi.current.value < 30 and self.adx.current.value > 20:
+            if self.bb.lower_band.current.value > price and self.rsi.current.value < 30 and self.adx.current.value > 20 and self.get_prediction() == "Up":
                 self.set_holdings(self.xauusd, 1)
                 self.plot("Trade Plot", "Buy", price)
                 self.exit_price = (1 - self.percentage_risk) * price
-            elif self.bb.upper_band.current.value < price and self.rsi.current.Value < 70 and self.adx.current.value > 20:
+            elif self.bb.upper_band.current.value < price and self.rsi.current.Value < 70 and self.adx.current.value > 20 and self.get_prediction() == "Down":
                 self.set_holdings(self.xauusd, -1)
                 self.plot("Trade Plot", "Sell", price)
                 self.exit_price = (1 + self.percentage_risk) * price
@@ -66,17 +90,37 @@ class SharedProject(QCAlgorithm):
             
 
     def on_data(self, data: Slice):
-        if not self.exit_price:
-            return
+        if self.exit_price:
+            price = data[self.xauusd].price
 
-        price = data[self.xauusd].price
-
-        if self.portfolio[self.xauusd].is_long:
-            if price < self.exit_price:
-                self.liquidate()
-                self.exit_price = None
+            if self.portfolio[self.xauusd].is_long:
+                if price < self.exit_price:
+                    self.liquidate()
+                    self.exit_price = None
+                    
+            else:
+                if price > self.exit_price:
+                    self.liquidate()
+                    self.exit_price = None
                 
+        
+
+
+    def get_prediction(self):
+        df = self.History(self.xauusd,40).loc[self.xauusd]
+        df_change = df[["open", "high", "low", "close"]].pct_change().dropna()
+        model_input = []
+        for index, row in df_change.tail(30).iterrows():
+            model_input.append(np.array(row))
+        model_input = np.array([model_input])
+
+        if round(self.model.predict(model_input)[0][0]) == 0:
+            return "Down"
         else:
-            if price > self.exit_price:
-                self.liquidate()
-                self.exit_price = None
+            return "Up"
+
+    def enable_trading(self):
+        self.is_trading_enabled = True
+
+    def disable_trading(self):
+        self.is_trading_enabled = False
